7d62a99385cf63b87e64ffc717c1cff8e5b5a1e5
alexisollwell <33710974+alexisollwell@users.noreply.github.com>
2024-04-25 17:10:59 -0700

    Add files via upload

---
+++ Services/church.backend/church.backend.services/Controllers/RolesController.cs
@@ -0,0 +1,36 @@
+﻿using church.backend.services.JsonWebToken;
+using church.backend.services.Models;
+using church.backend.services.Models.register;
+using church.backend.services.Models.roles;
+using church.backend.services.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace church.backend.services.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RolesServices _RolesServices;
+        public RolesController(RolesServices rolesServices)
+        {
+            _RolesServices = rolesServices;
+        }
+
+        [HttpGet]
+        [JwtAuthentication]
+        [Route("roles")]
+        public IActionResult consultRoles()
+        {
+            roles_response response = _RolesServices.consultRoles();
+            if (response.code != 1)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, response.message);
+            }
+            return Ok(response.data);
+        }
+
+    }
+}
