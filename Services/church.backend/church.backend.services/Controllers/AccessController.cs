ef34c5fa1348e0de5b924232388c2ad830a312b9
alexisollwell <33710974+alexisollwell@users.noreply.github.com>
2024-04-25 17:10:14 -0700

    Add files via upload

--- Services/church.backend/church.backend.services/Controllers/AccessController.cs
+++ Services/church.backend/church.backend.services/Controllers/AccessController.cs
@@ -1,8 +1,10 @@
 ﻿using church.backend.services.JsonWebToken;
 using church.backend.services.Models;
-using church.backend.services.Models.Client;
+using church.backend.services.Models.register;
 using church.backend.services.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -17,76 +19,53 @@ namespace church.backend.services.Controllers
         }
 
         [HttpGet]
-        [Route("client/get")]
-        public GeneralResponse GetClient([FromQuery] string email, string password)
-        {
-            return _AccessServices.GetClient(email, password);
-        }
-
-        [HttpPost]
-        [Route("client/save")]
-        public async Task<GetClientResponse> SaveClient([FromBody] SaveClientRequest request)
-        {
-            return await _AccessServices.SaveClient(request);
-        }
-
-        [HttpPut]
-        [Route("client/validate/")]
-        public GeneralResponse ValidateAccount([FromBody] ValidateAccountRequest request)
-        {
-            return _AccessServices.ValidateAccount(request);
-        }
-
-        [HttpGet]
-        [Route("client/sendCode/{email}/{brand}")]//XIGA services
-        public async Task<GeneralResponse> ReSendEmail([FromRoute] string email, [FromRoute] int brand)
-        {
-            return await _AccessServices.ReSendEmail(email, brand);
+        [Route("access/login")]
+        public IActionResult makeLogin([FromQuery] string email, string password)
+        {     
+            GeneralResponse client = _AccessServices.login(email, password);
+            if (client.code != 1) {
+                return StatusCode(StatusCodes.Status400BadRequest, client.message);
+            }
+            return Ok(client.message);
         }
 
         [HttpGet]
         [JwtAuthentication]
-        [Route("client/getPhoto")]
-        public GetClientResponse GetClientById()
+        [Route("access/update/password")]
+        public IActionResult ChangePassword([FromQuery] string password)
         {
             var claims = HttpContext.Items["Claims"] as IDictionary<string, string>;
-            int clientId = int.Parse(claims?["clientId"] ?? "0");
-            return _AccessServices.GetClientById(clientId);
-        }
-
-        [HttpPost]
-        [JwtAuthentication]
-        [Route("client/updatePhoto")]
-        public GeneralResponse UpdateClientPhoto([FromBody] UpdatePhotoRequest request)
-        {
-            var claims = HttpContext.Items["Claims"] as IDictionary<string, string>;
-            int clientId = int.Parse(claims?["clientId"] ?? "0");
-            return _AccessServices.UpdateClientPhoto(clientId, request.photo);
-        }
-
-        [HttpPost]
-        [JwtAuthentication]
-        [Route("client/changePassword")]
-        public GeneralResponse ChangePassword([FromQuery] string password)
-        {
-            var claims = HttpContext.Items["Claims"] as IDictionary<string, string>;
-            int clientId = int.Parse(claims?["clientId"] ?? "0");
-            return _AccessServices.ChangePassword(clientId,password);
+            int user_id = int.Parse(claims?["user_id"] ?? "0");
+            GeneralResponse response = _AccessServices.ChangePassword(user_id, password);
+            if (response.code != 1)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, response.message);
+            }
+            return Ok(response.message);
         }
 
         [HttpGet]
-        [Route("client/temporalPassword")]
-        public async Task<GeneralResponse> SetTempPassword([FromQuery] string email) //Change, must send html view via email
+        [Route("access/temporal/password")]
+        public async Task<IActionResult> SetTempPassword([FromQuery] string email)
         {
-            return await _AccessServices.SetTempPassword(email);
+            GeneralResponse response = await _AccessServices.SetTempPassword(email);
+            if (response.code != 1)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, response.message);
+            }
+            return Ok(response.message);
         }
 
         [HttpPost]
-        [JwtAuthentication]
-        [Route("client/updateProfile")]
-        public GetClientResponse UpdateProfile([FromBody] UpdateProfileRequest request)
+        [Route("access/create/employee")]
+        public IActionResult createEmployee([FromBody] create_employee_request data)
         {
-            return _AccessServices.UpdateProfile(request);
+            GeneralResponse client = _AccessServices.createEmployee(data);
+            if (client.code != 1)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, client.message);
+            }
+            return Ok(client.message);
         }
     }
 }
