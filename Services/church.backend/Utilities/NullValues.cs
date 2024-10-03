c589ff5dfe883af8fd6f8b40e3bdeab7b21b26be
alexisollwell <33710974+alexisollwell@users.noreply.github.com>
2024-10-03 08:52:54 -0700

    Add files via upload

---
+++ Services/church.backend/Utilities/NullValues.cs
@@ -0,0 +1,21 @@
+﻿namespace church.backend.Utilities
+{
+    public class NullValues
+    {
+        public int nullInt(string data) { try { return int.Parse(data); } catch { return 0; } }
+        public bool nullBool(string data)
+        {
+            return data.ToLower() == "true" ? true : false;
+        }
+        public bool nullBoolNumber(string data)
+        {
+            return data.ToLower() == "1" ? true : false;
+        }
+        public DateTime nullDate(string data) { try { return DateTime.Parse(data); } catch { return new DateTime(); } }
+        public double nullDouble(string data)
+        {
+            try { return double.Parse(data); }
+            catch { return 0.0; }
+        }
+    }
+}
