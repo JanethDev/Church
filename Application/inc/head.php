fac4d5175b5538c7ac80d1ed61280d39a60dcb1d
JanethDev <isc.janeth.ochoa@gmail.com>
2024-10-04 00:26:31 -0700

    update

--- Application/inc/head.php
+++ Application/inc/head.php
@@ -26,6 +26,9 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
+	<!-- Select2 -->
+	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 	
 
