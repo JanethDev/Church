2c351fd346069bf485be6694f96121e0f2e15f24
alexisollwell <33710974+alexisollwell@users.noreply.github.com>
2024-10-04 17:24:49 -0700

    Add files via upload

---
+++ Services/church.backend/Services/PurchaseServices.cs
@@ -0,0 +1,77 @@
+﻿using church.backend.DataBase;
+using church.backend.Models.catalogue.crypts;
+using church.backend.Models.catalogue.discounts;
+using church.backend.Models.purchase;
+using church.backend.services.DataBase;
+using church.backend.services.JsonWebToken;
+using church.backend.services.Models;
+
+namespace church.backend.Services
+{
+    public class PurchaseServices
+    {
+        private readonly cryptDB _cryptDB;
+        private readonly purchaseDB _purchaseDB;
+        private readonly discountDB _discountDB;
+
+        public PurchaseServices(purchaseDB purchaseDB, cryptDB cryptDB, discountDB discountDB)
+        {
+            _purchaseDB = purchaseDB;
+            _cryptDB = cryptDB;
+            _discountDB = discountDB;
+        }
+
+        public CurrencyResponse consultCurrencies()
+        {
+            return _purchaseDB.consultCurrencies();
+        }
+
+        public TypePaymentsResponse consultTypePayments()
+        {
+            return _purchaseDB.consultTypePayments();
+        }
+
+        public GeneralResponse CreatePurchase(purchase_request data, int userId)
+        {
+            if (data.customerId <= 0)
+            {
+                return new GeneralResponse()
+                {
+                    code = -1,
+                    message = "Es necesario enviar el id del cliente"
+                };
+            }
+
+            if (data.cryptId <= 0)
+            {
+                return new GeneralResponse()
+                {
+                    code = -1,
+                    message = "Es necesario enviar el id de la cripta"
+                };
+            }
+
+            crypt_response crypt = _cryptDB.consultById(data.cryptId);
+            if(crypt.data.Count == 0)
+            {
+                return new GeneralResponse()
+                {
+                    code = -1,
+                    message = "Es necesario enviar el id de la cripta valida"
+                };
+            }
+
+            data.cryptPrice = crypt.data[0].price;
+
+            if (data.discountId > 0)
+            {
+                discount_response temp = _discountDB.consultAll();
+                discount discount = temp.data.Find(item=>item.id == data.discountId)!;
+                data.discountAmount = discount.percentage*data.cryptPrice;
+            }
+            data.userId = userId;
+            data.datePurchase = DateTime.Now;
+            return _purchaseDB.CreatePurchase(data);
+        }
+    }
+}
