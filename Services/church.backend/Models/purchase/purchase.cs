b33e4f6c0b14712a7fa351e3f05ed589c833a071
alexisollwell <33710974+alexisollwell@users.noreply.github.com>
2024-10-24 14:45:52 -0700

    Update purchase.cs

--- Services/church.backend/Models/purchase/purchase.cs
+++ Services/church.backend/Models/purchase/purchase.cs
@@ -1,3 +1,4 @@
+using church.backend.Models.register;
 using church.backend.services.Models;
 
 namespace church.backend.Models.purchase
@@ -24,6 +25,39 @@ namespace church.backend.Models.purchase
         public string referencePersonPhone1 { get; set; } = string.Empty;
         public string referencePerson2 { get; set; } = string.Empty;
         public string referencePersonPhone2 { get; set; } = string.Empty;
+        public double otherFee { get; set; } = new double();
+        public string signature { get; set; } = string.Empty;
+
+        public int customerNumber {  get; set; } = new int();
+        public string customerName { get; set; } = string.Empty;
+        public string customerPsurname { get; set; } = string.Empty;
+        public string customerMsurname { get; set; } = string.Empty;
+        public string customerPhone { get; set; } = string.Empty;
+        public string customerEmail { get; set; } = string.Empty;
+        public string customerRFC { get; set; } = string.Empty;
+        public string customerZipCode { get; set; } = string.Empty;
+        public string customerAddress { get; set; } = string.Empty;
+        public int customerStateId {  get; set; } = new int();
+        public int customerTownId {  get; set; } = new int();
+        public string customerSocialReason { get; set; } = string.Empty;
+        public DateTime customerBirthdate { get; set; } = new DateTime();
+        public string customerBirthplace { get; set; } = string.Empty;
+        public string customerCivilStatus { get; set; } = string.Empty;
+        public string customerOccupation { get; set; } = string.Empty;
+        public string businessName { get; set; } = string.Empty;
+        public string businessAddress { get; set; } = string.Empty;
+        public string businessPhone { get; set; } = string.Empty;
+        public string businessExt { get; set; } = string.Empty;
+        public string deputation { get; set; } = string.Empty;
+        public double averageIncome { get; set; } = new double();
+        public string businessCity { get; set; } = string.Empty;
+        public string businessMunicipality { get; set; } = string.Empty;
+        public string businessState { get; set; } = string.Empty;
+        public string houseNumber { get; set; } = string.Empty;
+        public string aptNumber { get; set; } = string.Empty;
+        public string customerMunicipality { get; set; } = string.Empty;
+        public string neighborhood { get; set; } = string.Empty;
+        public List<Beneficiarie> beneficiaries { get; set; } = new List<Beneficiarie>();
         public List<Payment> payments { get; set; } = new List<Payment>();
     }
 }
