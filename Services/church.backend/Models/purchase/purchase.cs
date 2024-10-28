a22df72a1dc8bef044f53e97318c9f761c20cdd0
JanethDev <isc.janeth.ochoa@gmail.com>
2024-10-27 19:05:33 -0700

    Revert "update"
    This reverts commit 99670acdb8a3f974e319d89f1d1cdf334a8e73e4.

--- Services/church.backend/Models/purchase/purchase.cs
+++ Services/church.backend/Models/purchase/purchase.cs
@@ -11,7 +11,10 @@ namespace church.backend.Models.purchase
         public DateTime datePurchase { get; set; } = new DateTime();
         public int statusId {  get; set; } = new int();
         public string status { get; set; } = string.Empty;
-        public int cryptId {  get; set; } = new int(); 
+        public int cryptId {  get; set; } = new int();
+        public string fullPosition { get; set; } = string.Empty;
+        public string zone { get; set; } = string.Empty;
+        public string aisle { get; set; } = string.Empty;
         public double cryptPrice {  get; set; } = new double();
         public int cryptSpaces {  get; set; } = new int();
         public double maintenanceFee {  get; set; } = new double();
