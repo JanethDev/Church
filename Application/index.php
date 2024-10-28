32585a4bbeb20714421edb7029547bc5a832dc5a
JanethDev <isc.janeth.ochoa@gmail.com>
2024-10-28 10:55:56 -0700

    update

--- Application/index.php
+++ Application/index.php
@@ -7,24 +7,28 @@ include_once('inc/navbar.php');
 $currentPage = basename($_SERVER['PHP_SELF']);
 ?>
 <div class="container">
-    <div class="row justify-content-center">
-        <!-- Tarjeta 1 -->
-        <div class="col-md-3">
-            <div class="card text-center">
-                <img src="assets/img/Arquidiocesis_Color.png" class="card-img-top mx-auto" alt="Logo 1" style="width: 100px; height: 100px; margin-top: 10px;">
-                <div class="card-body">
-                    <h5 class="card-title">Parroquia 1</h5>
-                    <p class="card-text">Este es un texto de ejemplo para la tarjeta 1.</p>
+    <div class="row">
+        <div class="col-md-12" id="page-content">
+            <div class="row justify-content-center">
+                <!-- Tarjeta 1 -->
+                <div class="col-md-3">
+                    <div class="card text-center">
+                        <img src="assets/img/Arquidiocesis_Color.png" class="card-img-top mx-auto" alt="Logo 1" style="width: 100px; height: 100px; margin-top: 10px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Parroquia 1</h5>
+                            <p class="card-text">Este es un texto de ejemplo para la tarjeta 1.</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <!-- Tarjeta 2 -->
-        <div class="col-md-3">
-            <div class="card text-center">
-                <img src="assets/img/LOGO_CATEDRAL_TIJUANA.png" class="card-img-top mx-auto" alt="Logo 2" style="width: 100px; height: 100px; margin-top: 10px;">
-                <div class="card-body">
-                    <h5 class="card-title">Parroquia 2</h5>
-                    <p class="card-text">Este es un texto de ejemplo para la tarjeta 2.</p>
+                <!-- Tarjeta 2 -->
+                <div class="col-md-3">
+                    <div class="card text-center">
+                        <img src="assets/img/LOGO_CATEDRAL_TIJUANA.png" class="card-img-top mx-auto" alt="Logo 2" style="width: 100px; height: 100px; margin-top: 10px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Parroquia 2</h5>
+                            <p class="card-text">Este es un texto de ejemplo para la tarjeta 2.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
