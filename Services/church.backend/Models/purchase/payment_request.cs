f2b06fb4ce44bf370bec85db1ed8f1cd2d281737
alexisollwell <33710974+alexisollwell@users.noreply.github.com>
2024-11-07 12:43:31 -0800

    Update payment_request.cs

--- Services/church.backend/Models/purchase/payment_request.cs
+++ Services/church.backend/Models/purchase/payment_request.cs
@@ -1,5 +1,3 @@
-﻿using Newtonsoft.Json;
-
 namespace church.backend.Models.purchase
 {
     public class payment_request
@@ -8,14 +6,8 @@ namespace church.backend.Models.purchase
         public string concept { get; set; } = string.Empty;
         public int typePaymentId { get; set; } = new int();
         public int currencyId { get; set; } = new int();
-        [JsonIgnore]
-        [System.Text.Json.Serialization.JsonIgnore]
         public int number { get; set; } = new int();
-        [JsonIgnore]
-        [System.Text.Json.Serialization.JsonIgnore]
         public DateTime date { get; set; } = new DateTime();
-        [JsonIgnore]
-        [System.Text.Json.Serialization.JsonIgnore]
         public int statusId { get; set; } = new int();
     }
 }
